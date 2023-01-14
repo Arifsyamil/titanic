ffb8e61c885ab0f5041e1a9b5ec28f47ae98629e
Arifsyamil <121653152+Arifsyamil@users.noreply.github.com>
2023-01-14 17:40:54 +0800

    Update app.py

--- webpage/app.py
+++ webpage/app.py
@@ -16,7 +16,7 @@ def local_css(file_name):
 	with open(file_name) as f:
 		st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("webpage/style/style.css")
 
 #Load assets
 lottie_coding = load_lottieurl("https://assets8.lottiefiles.com/packages/lf20_o6spyjnc.json")
